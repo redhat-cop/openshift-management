b93e3abf65a8efac6a197b3c4c9e3db70448ed21
Øystein Bedin <bedin@redhat.com>
2018-01-05 04:16:37 +0000

    Cronjob for Pruning Projects

--- images/prune-ocp-projects/include/prune-ocp-projects.sh
+++ images/prune-ocp-projects/include/prune-ocp-projects.sh
@@ -1,41 +1,51 @@
 #!/bin/bash
-declare -a sys_exclude=("cluster-ops"  "kube-public" "logging")
-declare -a user_exclude=("infographic-delivery" "dantest")
+
+# Make sure to declare these two environment variables to prevent projects to be deleted
+# The values should be set as a quoted list of projects - i.e:
+# 'default openshift openshift-infra'
+#PROJECT_EXCLUDE_SYSTEM
+#PROJECT_EXCLUDE_USER
+
+# Use an indexed array to keep track of existing projects
 declare -A projects
-for project in `oc get project -o=custom-columns=NAME:.metadata.name --no-headers` ;
+
+for project in `oc get project -o=custom-columns=NAME:.metadata.name --no-headers`;
 do
-#  echo "found: ${project}"
-  projects[${project}]="found"
+  projects["${project}"]="found"
 done
 
-# delete the excluded
-for sys in "${sys_exclude[@]}" 
-do
-  unset projects[${sys}]
-#  echo "unset sys ${sys}"
-done
+# Eliminate the "System projects"
+if [ -n "${PROJECT_EXCLUDE_SYSTEM}" ];
+then
+  for project in ${PROJECT_EXCLUDE_SYSTEM};
+  do
+    unset projects["${project}"]
+  done
+fi
 
-for user in "${user_exclude[@]}" 
-do
-  unset projects[${user}]
-#  echo "unset user ${user}"
-done
+# Eliminate the "User projects"
+if [ -n "${PROJECT_EXCLUDE_USER}" ];
+then
+  for project in ${PROJECT_EXCLUDE_USER};
+  do
+    unset projects["${project}"]
+  done
+fi
 
-# capture time for each project
-for prj in "${!projects[@]}" 
+# Capture the timestamp for each project and only delete projects older
+# than the set number of hours
+for project in "${!projects[@]}";
 do
-# need variable for time
-  purgetime=`date -d "12" +%s`
-  temp=`oc get project ${prj} -o=custom-columns=time:.metadata.creationTimestamp --no-headers`
-  projects[${prj}]=`date -d "${temp}" +%s`
+  # need variable for time
+  purgetime=`date -d "${TIMESTAMP_HOURS_AGO}" +%s`
+  temp=`oc get project ${project} -o=custom-columns=time:.metadata.creationTimestamp --no-headers`
+  projects[${project}]=`date -d "${temp}" +%s`
 
-  echo "del: ${purgetime}: creationTimeEpocSec: ${prj}: ${projects[${prj}]}"
-  if [ $purgetime -gt ${projects[${prj}]} ]; then
+  echo "del: ${purgetime}: creationTimeEpocSec: ${project}: ${projects[${project}]}"
+  if [ ${purgetime} -gt ${projects[${project}]} ];
+  then
      echo "oc delete"
   else
      echo "No delete keep"
   fi
 done
-
-
-
